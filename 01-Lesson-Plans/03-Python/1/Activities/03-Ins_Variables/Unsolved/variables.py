4885a497652e877e2f5a91cb28b76a5759db862b
albinpierz@gmail.com <albinpierz@gmail.com>
2023-12-11 19:46:42 -0600

    can i do things correctly?

--- 01-Lesson-Plans/03-Python/1/Activities/03-Ins_Variables/Unsolved/variables.py
+++ 01-Lesson-Plans/03-Python/1/Activities/03-Ins_Variables/Unsolved/variables.py
@@ -1,13 +1,15 @@
 # Creates a variable with a string "Frankfurter"
-
+name= "frankfurter"
 # Creates a variable with an integer 80
-
+x=80
 # Creates a variable with the boolean value of True
-
+y= True
 # Prints a statement adding the variable
-
+z= 10
+print(x+z)
 # Convert the integer years into a string and prints
-
+print(str(x))
 # Converts a boolean into a string and prints
-
+print(str(y))
 # An f-string accepts all data types without conversion
+print(f"MyNameIS {name}It Is {x}It Is {y}It Is{z}")
--- 01-Lesson-Plans/03-Python/1/Activities/04-Stu_HelloVariableWorld/Unsolved/hello_variable_world.py
+++ 01-Lesson-Plans/03-Python/1/Activities/04-Stu_HelloVariableWorld/Unsolved/hello_variable_world.py
@@ -1,21 +1,22 @@
 # Create a variable called 'name' that holds a string
-
+name="albin"
 # Create a variable called 'country' that holds a string
-
+country="georgia"
 # Create a variable called 'age' that holds an integer
-
+age=34
 # Create a variable called 'hourly_wage' that holds an integer
-
+hourly_wage=17
 # Calculate the daily wage for the user
-
+daily_wage=8*hourly_wage
 # Create a variable called 'satisfied' that holds a boolean
-
+satisfied=True
 # Print out "Hello <name>!"
-
+print(f"Hello {name}!")
 # Print out what country the user entered
-
+print(f"{country}")
 # Print out the user's age
-
+print(f"{age}")
 # With an f-string, print out the daily wage that was calculated
-
+print(f"{daily_wage}")
 # With an f-string, print out whether the users were satisfied
+print(f"{satisfied}")
